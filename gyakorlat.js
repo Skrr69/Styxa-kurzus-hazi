6d7ab7e6a39f89a358c380360c25b14fd0714881
Turos Ferenc Akos <turosakos99@gmail.com>
2024-09-24 20:04:21 +0300

    gyakorlat styxa

---
+++ gyakorlat.js
@@ -0,0 +1,12 @@
+const szam2 = 2;
+let szam3 = 3;
+let valosszam = 3.14;
+let szoveg = "Hello
+let booleanvaltozo = false;
+
+cosole.log(booleanvaltozo);
+
+const tomb1 = [1, 2, 3, 4, 5];
+const keverttomb = [1, "Hello", true, 3.14]
+
+consele.log(keverttomb)
